7e2d6cb8221d9fa2b49db06130a244cd1fef5ee9
dbpatnodee <dbpatnode@hotmail.com>
2021-02-24 13:27:15 -0800

    bcrypt up and working

--- database.sql
+++ database.sql
@@ -2,12 +2,25 @@ CREATE DATABASE parktracker;
 
 CREATE TABLE users
 (
-    listitem_id SERIAL PRIMARY KEY,
-    name TEXT,
-    street TEXT,
-    city TEXT,
-    state TEXT,
-    zipcode TEXT,
-    naughty BOOLEAN,
-    nice BOOLEAN
+    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL
+
 );
+
+-- CREATE TABLE parks
+-- (
+--     park_id SERIAL PRIMARY KEY,
+--     user_id INT NOT NULL,
+--     street VARCHAR(40) NOT NULL,
+--     apartment VARCHAR(40),
+--     city VARCHAR(40),
+--     state VARCHAR(40),
+--     comments text,
+--     rating int NOT NULL,
+--     start_date DATE NOT NULL,
+--     end_date DATE NULL DEFAULT NULL,
+--     post_date DATE NOT NULL DEFAULT CURRENT_DATE,
+--     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+-- )
--- server.js
+++ server.js
@@ -1,61 +1,21 @@
 require('dotenv').config()
 const express = require('express');
 const app = express()
-// const cors = require('cors')
+const cors = require('cors')
 
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-// midleware for
-// app.use(cors());
+// MIDDLEWARE
+app.use(cors());
 app.use(express.json())
 
+// ROUTES
+
+// register and login routes
+app.use("/auth", require("./routes/jwtAuth"))
 
 
-// fake data (co0me back and add database here soon)
-const users = []
-
-app.post('/login', (req, res) => {
-    const username = req.body.username
-    user = {name: username}
-
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({accessToken: accessToken})
-})
-
-// authentication
-app.get('/users', (req, res) => {
-    res.json(users)
-})
-
-app.post('/users', async (req, res)=> {
-    try {
-        const salt = await bcrypt.genSalt()
-        const hashedPassword =  await bcrypt.hash(req.body.password, salt)
-        const user = { name: req.body.name, password: hashedPassword}
-        users.push(user)
-        res.status(201).send()
-    } catch {
-        res.status(500).send()
-    }
-})
-
-app.post('users/login', async (req, res) => {
-    const user = users.find(user => user.name = req.body.name)
-    if (user === null) {
-        return res.status(400).send('Cannot find user')
-        }
-        try {
-            if(await bcrypt.compare(req.body.password, user.password)) {
-                res.send('Success')
-            } else {
-                res.send('Not Allowed')
-            }
-        } catch {
-            res.status(500).send()
-        }
-})
-
-    app.listen(5000, () => {
+app.listen(5000, () => {
         console.log("Server has been started on port 5000")
-     })
+})
